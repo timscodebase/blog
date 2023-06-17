db9ad0739bd81853371da659a13bccc4edce9284
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-06-16 19:25:15 -0700

    Syntax Highlighting

--- svelte.config.js
+++ svelte.config.js
@@ -8,11 +8,19 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-import { mdsvex } from 'mdsvex'
+import { mdsvex, escapeSvelte } from 'mdsvex'
+import shiki from 'shiki'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await shiki.getHighlighter({ theme: 'poimandres' })
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+			return `{@html \`${html}\` }`
+		}
+	},
 }
 
 /** @type {import('@sveltejs/kit').Config} */
