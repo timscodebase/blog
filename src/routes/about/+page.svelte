1929279b5b276ae8d2bcb2419c35b4e9a1f59b2d
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-06-16 21:18:39 -0700

    pasges

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
 <h1>About</h1>
 
 <style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
 <h1>Contact</h1>
 
 <style>
