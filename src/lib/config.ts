bf61ea1871c8aaed8958b1da3c21fee898bd0a7a
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-07-07 21:53:46 -0700

    Readme

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -6,3 +6,13 @@ export const description = 'A simple Markdown blog built with SvelteKit'
 
 // This checks if we're in development mode and sets the URL accordingly
 export const url = dev ? 'http://localhost:5173/' : 'https://custom.url/'
+
+// SEO stuff
+export const author = 'Tim Smith'
+export const twitter = '@yourhandle'
+export const image = 'https://placekitten.com/1200/630'
+export const favicon = '/favicon.ico'
+export const keywords = 'svelte, sveltekit, blog, markdown'
+export const themeColor = '#000000'
+export const backgroundColor = '#ffffff'
+export const author_website = 'https://timsmith.tech'
